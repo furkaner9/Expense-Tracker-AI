8a4f4ce9b4370ea39901bcc0308741683c1529cd
Furkan ERİÇ <furkaneric9@gmail.com>
2025-08-07 16:48:05 +0300

    Complete Navbar components and guest about and contact pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import ClerkThemeProvider from "@/components/ClerkThemeProvider";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,15 +28,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+
+  
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider>
+          <ClerkThemeProvider>
         <Navbar />
         {children}
+        <Footer />
+        </ClerkThemeProvider>
+        </ThemeProvider>
       </body>
     </html>
-    </ClerkProvider>
   );
 }
